b6a83bd4ac2b31677fff10f2e7f38ceea0953b47
luizreboucas <primoneto@gmail.com>
2024-06-09 09:24:59 -0300

    api requests

---
+++ src/api/suppliers.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { Supplier } from "../usercases/Supplier";
+
+const request = axios.create({
+    baseURL: 'http://localhost:8080/api',
+    headers: {
+        "Content-Type": "application/sjon"
+    }
+})
+
+export const getAllSuppliers = () => {
+    const response = request.get("/suppliers");
+    return response;
+}
+
+export const getSupplier = (id: number) => {
+    const supplier = request.get(`/${id}`);
+    return supplier
+}
+
+export const updateSupplier = (supplierData: Supplier) =>{
+    const response = request.put(`/${supplierData.id}`, {
+        nome: supplierData.nome,
+        endereco: supplierData.endereco,
+        cnpj: supplierData.cnpj
+    })
+    return response;
+}
+
+export const deleteSupplier = (id: number) => {
+    const response = request.delete(`/${id}`);
+    return response
+}
---
+++ src/usercases/Supplier.ts
@@ -0,0 +1,53 @@
+import { deleteSupplier, getAllSuppliers, getSupplier, updateSupplier } from "../api/suppliers"; 
+export type Endereco = {
+    estado: string,
+    cidade: string,
+    logradouro: string,
+    numero: string
+}
+export interface Supplier {
+    id: number | undefined,
+    nome: string | undefined,
+    cnpj: string | undefined,
+    endereco: Endereco | undefined
+}
+
+export default class SupplierImpl implements Supplier{
+    id;
+    nome;
+    cnpj;
+    endereco;
+
+    constructor({id,nome,cnpj,endereco}: Supplier){
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+    }
+
+    static getSuppliers = async() => {
+        const response = await getAllSuppliers();
+        return response.data;
+    }
+
+    public getOneSupplier = async() => {
+        if(!this.id){
+            throw new Error("id de fornecedor não fornecido")
+        }
+        const response = await getSupplier(this.id);
+        return response.data;
+    }
+
+    public update = async() => {
+        const response = await updateSupplier(this);
+        return response.data
+    }
+
+    public delete = async() => {
+        if(!this.id){
+            throw new Error('fornecedor não encontrado')
+        }
+        const response =  await deleteSupplier(this.id);
+        return response.data;
+    }
+}
